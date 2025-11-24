c5edaba3b80d1bb965215c9c4e9c482b0e37fc1b
admodev <adolfohectormoyano@gmail.com>
2025-11-24 01:33:44 -0300

    feat: :sparkles: add custom themes support

--- src/texteditee/ui/renderer.py
+++ src/texteditee/ui/renderer.py
@@ -16,9 +16,29 @@ class Renderer:
         self.theme = theme_manager
         self.show_line_numbers = True
         self.highlighter = SyntaxHighlighter()
+        self.last_theme_name = None
+        self._apply_theme_background()
+    
+    def _apply_theme_background(self) -> None:
+        bg_color = self.theme.get_color('background')
+        bg_map = {
+            'black': '\x1b[40m',
+            'blue': '\x1b[44m',
+            'white': '\x1b[47m',
+            '#272822': '\x1b[40m',
+            '#002B36': '\x1b[40m',
+        }
+        
+        bg_code = bg_map.get(bg_color, '\x1b[40m')
+        print(bg_code, end='', flush=True)
         
     def render_buffer(self, buffer: 'Buffer', viewport: 'Viewport', 
                      cursor: 'Cursor', mode: 'Mode') -> None:
+        current_theme = self.theme.current_theme.name
+        if current_theme != self.last_theme_name:
+            self._apply_theme_background()
+            self.last_theme_name = current_theme
+            
         with self.term.hidden_cursor():
             print(self.term.home + self.term.clear, end='')
             
@@ -48,7 +68,21 @@ class Renderer:
         
         if self.show_line_numbers:
             line_num_str = str(line_num + 1).rjust(self._line_number_width(None))
-            output += self.term.cyan(line_num_str) + ' '
+            line_num_color = self.theme.get_color('line_number')
+            color_map = {
+                'cyan': self.term.cyan,
+                'yellow': self.term.yellow,
+                'bright_black': self.term.bright_black,
+                'white': self.term.white,
+                'red': self.term.red,
+                'green': self.term.green,
+                'magenta': self.term.magenta,
+                'blue': self.term.blue,
+                '#90908A': self.term.bright_black,
+                '#586E75': self.term.bright_black,
+            }
+            color_func = color_map.get(line_num_color, self.term.cyan)
+            output += color_func(line_num_str) + ' '
         
         if self.highlighter.enabled:
             tokens = self.highlighter.highlight_line(line)
@@ -73,25 +107,88 @@ class Renderer:
         padding = self.term.width - len(left_side) - len(right_side) - len(mode_str) - 2
         status_line = left_side + ' ' * max(0, padding) + mode_str + ' ' + right_side
         
-        with self.term.location(0, status_y):
-            print(self.term.on_blue(self.term.white(status_line[:self.term.width])))
-    
-    def _colorize(self, text: str, color_name: str) -> str:
-        color_map = {
-            'keyword': self.term.yellow,
-            'function': self.term.green,
-            'class': self.term.cyan,
-            'string': self.term.magenta,
-            'comment': self.term.bright_black,
-            'number': self.term.red,
-            'operator': self.term.white,
-            'builtin': self.term.cyan,
-            'decorator': self.term.yellow,
-            'default': self.term.white,
+        status_bg = self.theme.get_color('status_bg')
+        status_fg = self.theme.get_color('status_fg')
+        
+        bg_map = {
+            'black': 'black',
+            'blue': 'blue',
+            'white': 'white',
+            '#3E3D32': 'black',
+            '#073642': 'black',
+        }
+        fg_map = {
+            'white': self.term.white,
+            '#F8F8F2': self.term.white,
+            '#839496': self.term.white,
         }
         
-        color_func = color_map.get(color_name, self.term.white)
-        return color_func(text)
+        bg_name = bg_map.get(status_bg, 'blue')
+        fg_func = fg_map.get(status_fg, self.term.white)
+        
+        try:
+            bg_func = getattr(self.term, f'on_{bg_name}')
+            status_formatted = bg_func(fg_func(status_line[:self.term.width]))
+        except (AttributeError, TypeError):
+            status_formatted = self.term.on_blue(self.term.white(status_line[:self.term.width]))
+        
+        with self.term.location(0, status_y):
+            print(status_formatted)
+    
+    def _colorize(self, text: str, color_name: str) -> str:
+        color_attr_map = {
+            'keyword': 'keyword',
+            'function': 'function',
+            'class': 'class_name',
+            'string': 'string',
+            'comment': 'comment',
+            'number': 'number',
+            'operator': 'operator',
+            'builtin': 'builtin',
+            'decorator': 'decorator',
+            'default': 'foreground',
+        }
+        
+        attr_name = color_attr_map.get(color_name, 'foreground')
+        color = self.theme.get_color(attr_name)
+        
+        color_map = {
+            'black': self.term.black,
+            'red': self.term.red,
+            'green': self.term.green,
+            'yellow': self.term.yellow,
+            'blue': self.term.blue,
+            'magenta': self.term.magenta,
+            'cyan': self.term.cyan,
+            'white': self.term.white,
+            'bright_black': self.term.bright_black,
+            'bright_red': self.term.bright_red,
+            'bright_green': self.term.bright_green,
+            'bright_yellow': self.term.bright_yellow,
+            'bright_blue': self.term.bright_blue,
+            'bright_magenta': self.term.bright_magenta,
+            'bright_cyan': self.term.bright_cyan,
+            'bright_white': self.term.bright_white,
+            '#F92672': self.term.magenta,
+            '#A6E22E': self.term.green,
+            '#66D9EF': self.term.cyan,
+            '#E6DB74': self.term.yellow,
+            '#75715E': self.term.bright_black,
+            '#AE81FF': self.term.magenta,
+            '#268BD2': self.term.blue,
+            '#B58900': self.term.yellow,
+            '#2AA198': self.term.cyan,
+            '#859900': self.term.green,
+            '#D33682': self.term.magenta,
+            '#DC322F': self.term.red,
+            '#CB4B16': self.term.yellow,
+            '#F8F8F2': self.term.white,
+            '#839496': self.term.white,
+        }
+        
+        if color in color_map:
+            return color_map[color](text)
+        return text
     
     def _line_number_width(self, buffer: Optional['Buffer']) -> int:
         if buffer is None:
